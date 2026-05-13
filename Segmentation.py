2695297ab18907997d7de6278038b6b64ee6b8c5
Ozougwu Christian Chukwuebuka <100398222+Ozougwu@users.noreply.github.com>
2026-05-13 23:53:34 +0100

    Add files via upload

---
+++ Segmentation.py
@@ -0,0 +1,22 @@
+
+value = [
+    'Income', 
+    'Customer Lifetime Value',
+    'NetPoints',
+    'RedemptionRate'
+]
+
+frequency_and_volume = [
+    'DaysActive',
+    'AvgFlightsPerMonth',
+    'AvgDistancePerFlight',
+    'CompanionRatio'   
+]
+
+seasonal_patterns = [
+    'SummerFlightRatio', 
+    'WinterFlightRatio', 
+    'SpringFlightRatio', 
+    'AutumnFlightRatio',
+    'HolidayFlightRatio'
+]
